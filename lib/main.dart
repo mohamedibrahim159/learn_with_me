81bc3a68c50daa7ca90f9021a755f77f7e620441
mohamed <mohamed7ibrahim159@gmail.com>
2025-04-15 04:58:53 +0000

    feat: add app routes and update main file

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:learn_with_me/core/services/firebase_service.dart';
+import 'package:learn_with_me/presentation/routes/app_routes.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+    await FirebaseService.initialize();
+    runApp(const MyApp());
+  }
+
+  class MyApp extends StatelessWidget {
+    const MyApp({super.key});
+
+    @override
+    Widget build(BuildContext context) {
+      return MaterialApp(
+        routes: AppRoutes.routes,
+        initialRoute: AppRoutes.initialRoute,
+        home: const Text("Hello World"),
+      );
+    }
+  }
