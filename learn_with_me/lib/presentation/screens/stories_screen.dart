fecfc2ce41e0505b8860e64be96f899e0cc9bc1e
mohamed <mohamed7ibrahim159@gmail.com>
2025-04-17 17:09:13 +0000

    Make StoriesScreen responsive

--- learn_with_me/lib/presentation/screens/stories_screen.dart
+++ learn_with_me/lib/presentation/screens/stories_screen.dart
@@ -1,33 +1,74 @@
 import 'package:flutter/material.dart';
-import 'package:learn_with_me/core/constants/app_assets.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get_it/get_it.dart';
+import 'package:learn_with_me/domain/entities/story.dart';
+import 'package:learn_with_me/presentation/blocs/story_bloc.dart';
+import 'package:learn_with_me/presentation/widgets/responsive_widget.dart';
 
 class StoriesScreen extends StatelessWidget {
   const StoriesScreen({super.key});
-  final List<Map<String, String>> stories = const [
-    {'name': 'Story 1', 'image': AppAssets.storyImagePath},
-    {'name': 'Story 2', 'image': AppAssets.storyImagePath},
-    {'name': 'Story 3', 'image': AppAssets.storyImagePath},
-  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Stories Screen'),
-      ),
-      body: ListView.builder(
-        itemCount: stories.length,
-        itemBuilder: (context, index) {
-          final story = stories[index];
-          return ListTile(
-            leading: Image.asset(
-              story['image']!,
-              width: 50,
-              height: 50,
-            ),
-            title: Text(story['name']!),
-          );
-        },
+    return BlocProvider<StoryBloc>(
+      create: (context) =>
+          GetIt.I.get<StoryBloc>()..add(const GetStoriesEvent()),
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text('Stories'),
+        ),
+        body: BlocBuilder<StoryBloc, StoryState>(
+          builder: (context, state) {
+            if (state.isLoading) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (state.errorMessage != null) {
+              return Center(
+                child: Text(state.errorMessage!),
+              );
+            } else if (state.stories != null) {
+              return ResponsiveWidget(
+                mobileWidget: Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: ListView.builder(
+                    itemCount: state.stories!.length,
+                    itemBuilder: (context, index) {
+                      final story = state.stories![index];
+                      return ListTile(
+                        title: Text(story.name),
+                      );
+                    },
+                  ),
+                ),
+                tabletWidget: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                    ),
+                    itemCount: state.stories!.length,
+                    itemBuilder: (context, index) => Text(state.stories![index].name),
+                  ),
+                ),
+                desktopWidget: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: GridView.builder(
+                    gridDelegate:
+                        const SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 3,
+                    ),
+                    itemCount: state.stories!.length,
+                    itemBuilder: (context, index) => Text(state.stories![index].name),
+                  ),
+                ),
+              );
+            } else {
+              return const SizedBox();
+            }
+          },
+        ),
       ),
     );
   }
