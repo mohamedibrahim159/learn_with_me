d39baeca25f39d130229b1f952de98fc98d7009c
mohamed <mohamed7ibrahim159@gmail.com>
2025-04-17 17:13:10 +0000

    Make StoriesScreen responsive and login screen responsive and add simple design to it

--- learn_with_me/lib/presentation/screens/login_screen.dart
+++ learn_with_me/lib/presentation/screens/login_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:learn_with_me/presentation/blocs/auth_bloc.dart';
 import 'package:learn_with_me/presentation/routes/app_routes.dart';
+import 'package:learn_with_me/presentation/widgets/responsive_widget.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -11,10 +12,6 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return BlocProvider<AuthBloc>(
       create: (context) => GetIt.I.get<AuthBloc>(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Login Screen'),
-        ),
         body: BlocListener<AuthBloc, AuthState>(
           listener: (context, state) {
             if (state.user != null) {
@@ -28,13 +25,31 @@ class LoginScreen extends StatelessWidget {
               );
             }
           },
-          child: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                context.read<AuthBloc>().add(const AnonymousLoginEvent());
-              },
-              child: const Text('Login'),
+          child: ResponsiveWidget(
+            mobileWidget: _buildLoginButton(context),
+            tabletWidget: SizedBox(
+              width: 400,
+              child: _buildLoginButton(context),
             ),
+            desktopWidget: SizedBox(
+              width: 400,
+              child: _buildLoginButton(context),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildLoginButton(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Login as Guest'),
+          ElevatedButton(
+            onPressed: () {context.read<AuthBloc>().add(const AnonymousLoginEvent());},
+            child: const Text('Login'),
           ),
         ),
       ),
